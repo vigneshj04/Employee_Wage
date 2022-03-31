51aa8c632b6c1f80d6c8d657c7745c40a397881d
vigneshj04 <vigneshjmax@gmail.com>
2022-03-31 23:53:44 +0530

    Added: Employee Per Day Wage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,7 +3,9 @@ public class EmployeeWage {
     public static void main(String[] args) {
 
         empAttendance();
+        empDayWage();
     }
+
     static void empAttendance() {
 
         int IS_FULL_TIME = 1;
@@ -14,5 +16,21 @@ public class EmployeeWage {
             System.out.println("Employee is Absent");
         }
     }
+
+
+    public static void empDayWage() {
+        int Present = 1;
+        int Wage_per_Hour = 20;
+        int total_working_Hrs = 8;
+        double empCheck = Math.floor((Math.random() * 10) % 2);
+        if (Present == empCheck) {
+            System.out.println("Employee wage per day = " + total_working_Hrs * Wage_per_Hour);
+        } else
+            System.out.println("Employee is absent");
+    }
+
 }
+
+
+
 
