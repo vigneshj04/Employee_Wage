d9706a8ae10930b63a2192d3371138755dbe43e8
vigneshj04 <vigneshjmax@gmail.com>
2022-04-01 00:05:44 +0530

    Added: Employee Monthly Wage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -7,6 +7,7 @@ public class EmployeeWage {
         empWagePartTime();
         empSwitchCase();
         empWageDays();
+        employeeWageMontly();
     }
 
     static void empAttendance() {
@@ -102,6 +103,40 @@ public class EmployeeWage {
         System.out.println("Total Wage : " + totalWage);
     }
 
+    public static void employeeWageMontly() {
+
+        final int IS_PART_TIME = 1;
+        final int IS_FULL_TIME = 2;
+        final int EMP_RATE_PER_HOUR = 20;
+        final int NUM_OF_WORKING_DAYS = 20;
+        final int MAXI_HRS_IN_MONTH = 100;
+
+        int empWage = 0;
+        int totalWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+
+        while (totalEmpHrs <= MAXI_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            int empHrs = 0;
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalEmpHrs += empHrs;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalWage += empWage;
+            System.out.println("Emp wage = " + empWage);
+        }
+        System.out.println("Total Wage : " + totalWage);
+    }
 }
 
 
